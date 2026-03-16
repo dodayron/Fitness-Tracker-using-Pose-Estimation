70acd6bd8c0c367d5c13f248f48c1c6a85cfdd96
Aaron <163346151+dodayron@users.noreply.github.com>
2026-03-16 20:47:55 +0000

    Update exercise_v2.py
    added lateral raise plus direction
    added additional conditions to the state machine to check form

--- exercise_v2.py
+++ exercise_v2.py
@@ -2,14 +2,16 @@ import numpy as np
 import mediapipe as mp
 
 mp_pose = mp.solutions.pose
+vis_thresh = 0.6 # threshold for landmark detection
 
 class Exercise:
     def __init__(self, config):
         self.name = config["name"]
         self.angle_points = config["angle_points"]
         self.sides = config["sides"]
-        # rest_thresh = Angle when limb is extended (e.g., Standing for squat)
-        # peak_thresh = Angle at full contraction (e.g., Deep squat)
+        self.direction = config["direction"]
+        # rest_thresh = Angle at limb start point (e.g., Standing for squat)
+        # peak_thresh = Angle at limb end point (e.g., Deep squat)
         self.rest_thresh = config["thresholds"]["rest"]
         self.peak_thresh = config["thresholds"]["peak"]
         
@@ -25,8 +27,8 @@ class Exercise:
         b = np.array([b.x, b.y])
         c = np.array([c.x, c.y])
 
-        radians = (np.arctan2(c[1]-b[1], c[0]-b[0]) - 
-                  np.arctan2(a[1]-b[1], a[0]-b[0]))
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+                  np.arctan2(a[1]-b[1], a[0]-b[0])
         angle = abs(radians * 180.0 / np.pi)
         
         if angle > 180.0:
@@ -41,6 +43,11 @@ class Exercise:
                 p1 = self._get_point(landmarks, side, self.angle_points[0])
                 p2 = self._get_point(landmarks, side, self.angle_points[1])
                 p3 = self._get_point(landmarks, side, self.angle_points[2])
+
+                # calc if landmarks pass threshold
+                if min (p1.visibility, p2.visibility, p3.visibility) < vis_thresh:
+                        continue
+
                 angles.append(self._calculate_angle(p1, p2, p3))
             except (AttributeError, IndexError):
                 pass # Landmark not visible
@@ -54,16 +61,57 @@ class Exercise:
 
         if angle is None:
             return None
+            
+        #curl validation - check if wrist is above elbow at peak / skip other exercises
+        if "curl" in self.name.lower():
+                try:
+                    side = self.sides[0] # left or right
+                    elbow = self._get_point(landmarks, side, "ELBOW")
+                    wrist = self._get_point(landmarks, side, "WRIST")
+                    wrist_raised = wrist.y < elbow.y #cuz y increases downwards
+                except:
+                        wrist_raised = False # assume false if cant detect wrist
+        else:
+            wrist_raised = True # set true if its a non curl exercise so later ifs pass
+            
+        #lat raise validation - check wrist is above elbows when performing exercise
+        if "lateral raise" in self.name.lower():
+                try:
+                    side = self.sides[0] # left or right
+                    shoulder = self._get_point(landmarks, side, "SHOULDER")
+                    elbow = self._get_point(landmarks, side, "ELBOW")
+                    wrist = self._get_point(landmarks, side, "WRIST")
+                    arm_raised = (
+                        elbow.y < shoulder.y and        # elbow must be above shoulder
+                        wrist.y >= elbow.y - 0.05     #check if wrist near the height of elbow
+                        )
+                except:
+                    arm_raised = False # if cant detect elbow and wrist
+        else:
+            arm_raised = True # set true if non lateral raise exercise
 
         # State Machine: Rest -> Peak -> Rest
+        # 0. check direction is increase
+        if self.direction == "increase":
+            # 1. check if angle above peak
+            if angle > self.peak_thresh:
+                if arm_raised:
+                    self.stage = "peak"
+            # 2. chcek angle returns to rest after
+            if angle < self.rest_thresh and self.stage == "peak":
+                self.stage = "rest"
+                self.reps += 1
         
-        # 1. Check if we reached the Peak
-        if angle < self.peak_thresh:
-            self.stage = "peak"
+        #0. direction is decrease        
+        else:        
+            # 1. Check if we reached Peak
+            if angle < self.peak_thresh:
+                if wrist_raised and arm_raised: #check for curls/lat raise, wont affect other exercises
+                    self.stage = "peak"
             
-        # 2. Check if we returned to Rest AND were previously at peak
-        if angle > self.rest_thresh and self.stage == "peak":
-            self.stage = "rest"
-            self.reps += 1
+            # 2. Check if we returned to Rest AND were at peak before
+            if angle > self.rest_thresh and self.stage == "peak":
+                self.stage = "rest"
+                self.reps += 1
 
         return angle
